636c4cf1894f3364268914441f8a268886fcaa0a
Charlotte Saethre <csaethre@bruinmail.slcc.edu>
2021-02-10 08:07:34 -0700

    Adds skeleton classes with doc comments

---
+++ Project1/src/project1/BooleanSets.java
@@ -0,0 +1,87 @@
+package project1;
+
+/**
+ * 
+ * @author William Norton, Marshall Ringwood, Charlotte Saethre, 
+ * Cody Salmond, Jeremiah Smith, & Alvaro Zarate
+ *
+ */
+public class BooleanSets {
+
+	/**
+	 * Performs union operation on <code>setA</code> and <code>setB</code> and
+	 * returns the result as a boolean array.
+	 * 
+	 * @param setA first set of union
+	 * @param setB second set of union
+	 * @return union of the two sets
+	 */
+	public static boolean[] Union(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Performs intersection operation on <code>setA</code> and <code>setB</code>
+	 * and returns the result as a boolean array.
+	 * 
+	 * @param setA first set of intersection
+	 * @param setB second set of intersection
+	 * @return intersection of the two sets
+	 */
+	public static boolean[] Intersection(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Computes complement of a <code>setA</code> and returns a the result as a
+	 * boolean array.
+	 * 
+	 * @param setA set to compute complement
+	 * @return complement of set
+	 */
+	public static boolean[] Complement(boolean[] setA) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Performs addition operation on <code>setA</code> and <code>setB</code> and
+	 * returns the result as a boolean array.
+	 * 
+	 * @param setA first set of addition
+	 * @param setB second set of addition
+	 * @return the sum of the sets
+	 */
+	public static boolean[] Add(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Performs subtraction operation on <code>setA</code> and <code>setB</code> and
+	 * returns the result as a boolean array.
+	 * 
+	 * @param setA first set of subtraction
+	 * @param setB second set of subtraction
+	 * @return the difference of the sets
+	 */
+	public static boolean[] Subtract(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Performs Xor operation on <code>setA</code> and <code>setB</code> and returns
+	 * the result as a boolean array.
+	 * 
+	 * @param setA first set of Xor operation
+	 * @param setB second set of Xor operation
+	 * @return result of Xor operation on both sets
+	 */
+	public static boolean[] Xor(boolean[] setA, boolean[] setB) {
+		return null;
+	}
+
+}
---
+++ Project1/src/project1/Client.java
@@ -0,0 +1,70 @@
+package project1;
+
+import java.io.IOException;
+/**
+ * 
+ * @author William Norton, Marshall Ringwood, Charlotte Saethre, 
+ * Cody Salmond, Jeremiah Smith, & Alvaro Zarate
+ * 
+ */
+public class Client {
+
+	public static void main(String[] args) {
+
+		int selection = 0;
+		System.out.println("Welcome to Programming Project 1. Please enter '1', '2', or '3' to select a problem.");
+		try {
+			selection = System.in.read();// read an integer from the console
+		} catch (IOException e) {
+			System.out.println("Invalid Input");
+		}
+		switch (selection) {// switch on console input
+		case 1: {
+			Problem1();// run problem number 1
+
+			break;
+		}
+		case 2: {
+			Problem2();// run problem number 2
+			break;
+		}
+		case 3: {
+
+			Problem3();// run problem number 3
+			break;
+		}
+
+		default:
+			throw new IllegalStateException("Something Horrible has Happened");
+		}
+
+	}
+
+	/**
+	 * Performs set operations on given subsets A and B of a set with n elements.
+	 * Set Operations: Complement, Union, Intersection, Difference, and Xor.
+	 * 
+	 */
+	public static void Problem1() {
+		// TODO
+	}
+
+	/**
+	 * Performs set operations on multisets A and B from same universal set.
+	 * Set Operations: Union, Intersection, Difference, and Addition.
+	 * 
+	 */
+	public static void Problem2() {
+		// TODO
+	}
+
+	/**
+	 * Performs set operations on fuzzy sets A and B.
+	 * Set Operations: Complement, Union, and Intersection.
+	 * 
+	 */
+	public static void Problem3() {
+		// TODO
+	}
+
+}
