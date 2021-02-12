f6039cc003eb5cee0f5b260a3813d994987b5208
Charlotte Saethre <csaethre@bruinmail.slcc.edu>
2021-02-11 19:52:54 -0700

    Adds class FuzzySets

---
+++ Project1/src/project1/FuzzySets.java
@@ -0,0 +1,47 @@
+package project1;
+/**
+ * 
+ * @author William Norton, Marshall Ringwood, Charlotte Saethre, 
+ * Cody Salmond, Jeremiah Smith, & Alvaro Zarate
+ *
+ */
+public class FuzzySets {
+	/**
+	 * Performs union operation on <code>setA</code> and <code>setB</code> and
+	 * returns the result as a boolean array.
+	 * 
+	 * @param setA first set of union
+	 * @param setB second set of union
+	 * @return union of the two sets
+	 */
+	public static boolean[] Union(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Performs intersection operation on <code>setA</code> and <code>setB</code>
+	 * and returns the result as a boolean array.
+	 * 
+	 * @param setA first set of intersection
+	 * @param setB second set of intersection
+	 * @return intersection of the two sets
+	 */
+	public static boolean[] Intersection(boolean[] setA, boolean[] setB) {
+		// TODO
+		return null;
+	}
+
+	/**
+	 * Computes complement of a <code>setA</code> and returns a the result as a
+	 * boolean array.
+	 * 
+	 * @param setA set to compute complement
+	 * @return complement of set
+	 */
+	public static boolean[] Complement(boolean[] setA) {
+		// TODO
+		return null;
+	}
+
+}
