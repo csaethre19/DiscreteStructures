7b16abce6e7ddd990250551dc6fb6b95b3c6979c
Charlotte Saethre <csaethre@bruinmail.slcc.edu>
2021-02-22 19:51:09 -0700

    adds project for Project2

---
+++ Project2/src/project2/Client.java
@@ -0,0 +1,16 @@
+package project2;
+
+/**
+ * 
+ * 
+ * @author William Norton, Marshall Ringwood, Charlotte Saethre, 
+ * Cody Salmond, & Jeremiah Smith
+ *
+ */
+public class Client {
+
+	public static void main(String[] args) {
+
+	}
+
+}
