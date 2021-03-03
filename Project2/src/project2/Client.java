9a9855a27c3a2df543ef4a328d76b5b60f0b8b27
Charlotte Saethre <csaethre@bruinmail.slcc.edu>
2021-03-02 19:22:14 -0700

    Adds sort classes from algs4 and TODOs in Client

--- Project2/src/project2/Client.java
+++ Project2/src/project2/Client.java
@@ -1,7 +1,7 @@
 package project2;
 
 /**
- * 
+ * Test Driver
  * 
  * @author William Norton, Marshall Ringwood, Charlotte Saethre, 
  * Cody Salmond, & Jeremiah Smith
@@ -10,7 +10,15 @@ package project2;
 public class Client {
 
 	public static void main(String[] args) {
-
+		// Example
+		Integer[] a = {3, 2, 0, 1};
+		Merge.sort(a);
+		Quick.sort(a);
+		Heap.sort(a);
+		// TODO
+		// Need Shaker Sort class code
+		// Need array generator for all permutations based on N
+		// Need to add methods to sort classes that keeps track of comparisons made
 	}
 
 }
