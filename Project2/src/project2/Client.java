fb638bbf0d305839297646ed61495b6368990b06
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2021-03-09 16:22:31 -0700

    Quick and Perm Generator
    Finished quick comparisons and added an unfinished permutation generator

--- Project2/src/project2/Client.java
+++ Project2/src/project2/Client.java
@@ -25,7 +25,51 @@ public class Client {
 		System.out.println("Comparison b: " + heap.getCount());
 		// TODO
 		// Need Shaker Sort class code
+
+		
 		// Need array generator for all permutations based on N
+		//This is a rough generator for part two
+		int n = 5;
+        int[] perms = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+			perms[i] = i;
+		}
+        
+        Client c = new Client();
+       c.permutations(perms, perms.length, perms.length);
+        
+     }
+     
+     public void permutations(int[] a, int size, int n){
+         
+         if(size == 1) printArray(a, n);
+         
+         for (int i = 0; i < size; i++){
+             
+             permutations(a, size - 1, n);
+             
+             if(size % 2 == 1){
+                 int temp = a[0];
+                 a[0] = a[size-1];
+                 a[size-1] = temp;
+             }
+             
+             if(size % 2 == 0 ){
+                 int temp = a[i];
+                 a[i] = a[size-1];
+                 a[size-1] = a[i];
+             }
+         }
+     }
+    
+     public void printArray(int[] a, int n){
+         
+         for (int i = 0; i < a.length; i++){
+             System.out.print(a[i] + " ");
+         }
+     }
+
 		// Need to add methods to sort classes that keeps track of comparisons made
 	}
 
