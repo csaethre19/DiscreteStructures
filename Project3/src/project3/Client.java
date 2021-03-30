46651041af76dbbc4fcdfa030d1fa790f91db9fd
Charlotte Saethre <csaethre@bruinmail.slcc.edu>
2021-03-29 20:26:13 -0600

    Adds Project3

---
+++ Project3/src/project3/Client.java
@@ -0,0 +1,37 @@
+package project3;
+
+/**
+ * The question you are to investigate is what combination of payloads gives the
+ * highest overall rating
+ * 
+ * DATA:
+ * 
+ * Number	Experiment			Weight	Rating
+ * ------------------------------------------
+ * 1		Cloud Patterns		36		5
+ * 2		Solar Flares		264		9
+ * 3		Solar Power			188		6
+ * 4		Binary Stars		203		8
+ * 5		Relativity			104		8
+ * 6		Seed Viability		7		4
+ * 7		Sun Spots			90		2
+ * 8		Mice Tumors			65		8
+ * 9		Microgravit...		75		5
+ * 10		Micrometerorites	170		9
+ * 11		Cosmic Rays			80		7
+ * 12		Yeast Fermentation	27		4
+ * 
+ * @author William Norton, Marshall Ringwood, Charlotte Saethre, Cody Salmond, &
+ *         Jeremiah Smith
+ *
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		// 1. chose items with the highest rating first
+		// 2. select items from the lightest to the most massive
+		// 3. select experiments based on the ratio of rating to mass
+		// 4. brute force method using all the permutations
+	}
+
+}
